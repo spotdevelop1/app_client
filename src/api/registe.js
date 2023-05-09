b73ea89d5abda0cc79ead8a830e98d3fc0006cd0
Carlos Andres Morales Banda <c.banda07@hotmail.com>
2023-05-09 11:56:25 -0600

    update url sigIn

--- src/api/registe.js
+++ src/api/registe.js
@@ -1,6 +1,6 @@
 export async function registerApi(name, email, phone, password, passwordConfirm){
     try {
-        const response = await fetch('https://appmobile.altcel2.com/registro', {
+        const response = await fetch('https://appmobile.altcel2.com/signIn', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
