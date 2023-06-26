710fda6c50ffe7c7f813d83dba9e1a4b5759118d
CarlosBanda <c.banda07@hotmail.com>
2023-06-26 10:08:19 -0600

    consumos services

--- src/api/altan.js
+++ src/api/altan.js
@@ -16,12 +16,13 @@ export async function accessTokenRequestPost(){
 
 export async function consultUF(service, number){
     // console.log(number+' UF')
+    // return false
     try {
         const response = await fetch('https://apps-ws.spot1.mx/consultUf?msisdn='+number+'&product=MOV',{
             method: 'GET'
         })
         const data = await response.json()
-        console.log(data.consultUF)
+        console.log(data.status + ' UF')
         return data
     } catch (error) {
         return error
